9c4203dfd338fa8ac63b2eda8a0b5b0e78c84dad
unknown <gaolingfeng@hesaitech.com>
2022-07-22 14:22:45 +0800

    test.v submit

---
+++ test.v
@@ -0,0 +1,29 @@
+'timescale 1ns\1ps
+/***********************************************
+#
+#      Filename: test.v
+#
+#        Author: Clough - clough@gmail.com
+#   Description: ---
+#        Create: 2022-07-21 15:36:44
+# Last Modified: 2022-07-21 17:31:04
+***********************************************/
+
+begin
+    if( stream_out = 1'b1 ) begin
+        
+    end
+    else if() begin
+
+    end
+end
+
+begin
+    
+end
+always @ (*)
+begin
+    if()
+end
+
+ 
